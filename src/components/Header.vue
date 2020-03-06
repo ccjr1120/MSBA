1661a3320d67d0cd0743426a0648a411381b4999
h0341a <qianaiwuyu@gmail.com>
2020-03-06 10:40:30 +0800

    page header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,31 @@
 <template>
-    <el-page-header @back="goBack" content="详情页面"></el-page-header>
+  <el-header height="56px">
+    <el-page-header v-if="pageInfo.parent != null" style="float:left; margin-top:16px" @back="goBack" title="" :content="pageInfo.name"></el-page-header>
+    <span v-else style="font-size:18px;">{{pageInfo.name}}</span>
+    <div style="float:right; margin-top:10px">
+      <el-popover placement="bottom" v-model="popoverVisible">
+        <div style="text-align: right; margin: 0;">
+          <el-button size="mini" type="text" @click="popoverVisible = false">个人简介</el-button>
+          <br />
+          <el-button type="text" size="mini">注销</el-button>
+        </div>
+        <el-avatar slot="reference" :size="40"></el-avatar>
+      </el-popover>
+    </div>
+  </el-header>
 </template>
 <script>
-  export default {
-    methods: {
-      goBack() {
-        console.log('go back');
-      }
+export default {
+  props:['pageInfo'],
+  data() {
+    return {
+      popoverVisible: false,
+    }
+  },
+  methods: {
+    goBack() {
+      console.log("go back");
     }
   }
+};
 </script>
