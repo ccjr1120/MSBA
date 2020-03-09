ae4b7df735a36a62e6ba8a9cb93ec925f115f8f8
h0341a <qianaiwuyu@gmail.com>
2020-03-09 16:56:33 +0800

    Get recursive component of child comment. UI and name needs to be optimizecd

---
+++ src/components/ChildComment.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{comment.nickname}} say: {{comment.content}}
+    <div style="padding-left:20px;" v-bind:key="child.label" v-for="child in comment.children">
+      <ChildComment v-bind:comment="child"></ChildComment>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChildComment",
+  props: ["comment"]
+};
+</script>
+
+<style>
+</style>
--- src/views/Comment.vue
+++ src/views/Comment.vue
@@ -3,15 +3,25 @@
     <Header v-bind:pageInfo="pageInfo"></Header>
     <el-main>
       <el-table :data="tableData" border style="width: 100%">
-        <el-table-column prop="cid" label="评论ID" width="180"></el-table-column>
-        <el-table-column prop="parent" label="是否为父评论" width="180"></el-table-column>
+        <el-table-column prop="nickname" label="评论昵称" width="180"></el-table-column>
+        <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
         <el-table-column prop="createDate" label="日期" width="180"></el-table-column>
         <el-table-column prop="blogTitle" label="博客标题"></el-table-column>
         <el-table-column prop="content" label="评论内容"></el-table-column>
-        <el-table-column fixed="right" label="操作" width="200">
+        <el-table-column label="操作" width="200">
           <template slot-scope="scope">
-            <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">回复</el-button>
-            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            <el-button
+              v-if="scope.row.children!=null"
+              size="mini"
+              type="primary"
+              @click="dialogVisible = true"
+            >查看对话</el-button>
+            <el-button v-else size="mini" type="primary">回复</el-button>
+            <el-button size="mini" type="danger">删除</el-button>
+            <el-dialog title="查看对话" :visible.sync="dialogVisible" width="30%">
+              <ChildComment v-bind:comment="scope.row"></ChildComment>
+              <span slot="footer" class="dialog-footer"></span>
+            </el-dialog>
           </template>
         </el-table-column>
       </el-table>
@@ -19,23 +29,52 @@
   </div>
 </template>
 <script>
+import ChildComment from "../components/ChildComment";
+
 export default {
   data() {
     return {
+      dialogVisible: false,
       pageInfo: {
         name: "评论列表",
         parent: "/home"
       },
       tableData: [
         {
-          cid: "1",
-          parent: "true",
+          cid: 1,
+          nickname: "nichname",
+          email: "1110@qq.com",
           createDate: "2016-05-02",
-          content: "comment example1",
-          blogTitle: "blog title example1"
+          blogTitle: "blog title example1",
+          content: "评论",
+          children: [
+            {
+              cid: 1,
+              nickname: "nichname",
+              email: "1110@qq.com",
+              createDate: "2016-05-02",
+              blogTitle: "blog title example1",
+              content: "评论的评论",
+              children: [
+                {
+                  cid: 1,
+                  nickname: "nichname",
+                  email: "1110@qq.com",
+                  createDate: "2016-05-02",
+                  blogTitle: "blog title example1",
+                  content: "评论的评论的评论",
+                  children: []
+                }
+              ]
+            }
+          ]
         }
       ]
     };
+  },
+  methods: {},
+  components: {
+    ChildComment
   }
 };
 </script>
