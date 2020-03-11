2ca4661be2d1cec4b2e3e89d6ca24a7f2f412209
h0341a <qianaiwuyu@gmail.com>
2020-03-11 10:16:01 +0800

    Add delete blog function for article list page

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -2,7 +2,7 @@
   <div class="fillcontain">
     <Header v-bind:pageInfo="getPageName(this.$route.path)"></Header>
     <el-main>
-      <el-table :data="blogList" border style="width: 100%">
+      <el-table :data="articleList" border style="width: 100%">
         <el-table-column prop="bid" label="博客ID" width="180"></el-table-column>
         <el-table-column prop="category" label="分类" width="180"></el-table-column>
         <el-table-column prop="createDate" label="创建日期" width="180"></el-table-column>
@@ -10,7 +10,7 @@
         <el-table-column fixed="right" label="操作" width="200">
           <template slot-scope="scope">
             <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            <el-button size="mini" type="danger" @click="handleDelete(scope.$index)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -25,7 +25,7 @@ export default {
         name: "文章列表·all",
         parent: "/home"
       },
-      blogList: [
+      articleList: [
         {
           bid: "1",
           category: "未分类",
@@ -40,9 +40,9 @@ export default {
     getPageName: path => {
       //path's examples:"/articles", "/articles/category"...
       var pageInfo = {
-          parent:"/home",
-          name:""
-      }
+        parent: "/home",
+        name: ""
+      };
       var pageName = "文章列表·";
       if (path === "/articles") {
         pageName += "all";
@@ -50,7 +50,30 @@ export default {
         pageName += path.substring(10);
       }
       pageInfo.name = pageName;
-      return pageInfo
+      return pageInfo;
+    },
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index) {
+      this.$confirm("此操作将会删除该博客, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.articleList.splice(index, 1);
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
     }
   }
 };
