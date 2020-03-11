a590578c3c88a43fd43110dda363199d26892d2d
h0341a <qianaiwuyu@gmail.com>
2020-03-11 10:04:41 +0800

    Now, more infomation needs to be input on the ArticleEdit page

--- src/views/ArticleEdit.vue
+++ src/views/ArticleEdit.vue
@@ -2,6 +2,38 @@
   <div class="fillcontain">
     <Header v-bind:pageInfo="pageInfo"></Header>
     <el-main style="height:90%;overflow:visible;">
+      <el-dialog title="文章基础信息" :visible.sync="dialogVisible">
+        <el-form :model="article" ref="articleForm" :rules="rules">
+          <el-form-item label="分类" prop="category" label-width="120px">
+            <el-select
+              v-model="article.category"
+              filterable
+              allow-create
+              default-first-option
+              placeholder="请选择文章标签(可自建)"
+            >
+              <el-option v-for="category in categoryList" :key="category" :value="category"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="标题" prop="title" label-width="120px">
+            <el-input maxlength="24" show-word-limit v-model="article.title" autocomplete="off"></el-input>
+          </el-form-item>
+
+          <el-form-item label="描述" label-width="120px">
+            <el-input
+              type="textarea"
+              maxlength="64"
+              show-word-limit
+              v-model="article.description"
+              placeholder="选填"
+              autocomplete="off"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button type="primary" @click="checkForm('articleForm')">确 定</el-button>
+        </div>
+      </el-dialog>
       <div id="editor">
         <mavon-editor style="height: 100%"></mavon-editor>
       </div>
@@ -16,10 +48,34 @@ export default {
   data() {
     return {
       pageInfo: {
-        name: "博客编辑",
+        name: "新建博客",
         parent: "/home"
+      },
+      dialogVisible: true,
+      categoryList: ["未命名", "测试分组"],
+      rules: {
+        title: [{ required: true, message: "请输入标题", trigger: "blur" }],
+        category: [{ required: true, message: "请选择分类", trigger: "blur" }]
+      },
+      article: {
+        title: "",
+        category: "",
+        content: "",
+        description: ""
       }
     };
+  },
+  methods: {
+    checkForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.dialogVisible = false;
+        } else {
+          window.console.log("error submit!!");
+          return false;
+        }
+      });
+    }
   },
   components: {
     mavonEditor
