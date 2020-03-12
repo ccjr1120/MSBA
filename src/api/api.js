8779fcb54b2f349dd8b9a810bae6fde3b6c22a3f
h0341a <qianaiwuyu@gmail.com>
2020-03-12 19:04:39 +0800

    Get comment list from the end-back api. And to use in comment.vue

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,22 @@
 import axios from 'axios';
 import Qs from 'qs';
 axios.defaults.baseURL = 'http://localhost:8090/api/admin/'; //default url
-axios.defaults.withCredentials=true;
+axios.defaults.withCredentials = true;
 // request interceptor
 axios.interceptors.request.use(config => {
-  config.data = Qs.stringify(config.data); // handle request data
-  return config;
+    config.data = Qs.stringify(config.data); // handle request data
+    return config;
 },
-  err => {
-    return Promise.reject(err);
-  });
+    err => {
+        return Promise.reject(err);
+    });
 
+//api request
+
+export const getArticleList = () => {
+    return axios.get('/blogList');
+}
+
+export const getCommentList= ()=>{
+    return axios.get('/comments')
+}
--- src/views/Comment.vue
+++ src/views/Comment.vue
@@ -2,7 +2,7 @@
   <div class="fillcontain">
     <Header v-bind:pageInfo="pageInfo"></Header>
     <el-main>
-      <el-table :data="tableData" border style="width: 100%">
+      <el-table :data="commentList" border style="width: 100%">
         <el-table-column prop="nickname" label="评论昵称" width="180"></el-table-column>
         <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
         <el-table-column prop="createDate" label="日期" width="180"></el-table-column>
@@ -14,97 +14,51 @@
               v-if="scope.row.children!=null"
               size="mini"
               type="primary"
-              @click="dialogVisible = true"
+              @click="showDialogView(scope.row)"
             >查看对话</el-button>
             <el-button v-else size="mini" type="primary">回复</el-button>
-            <el-button size="mini" type="danger">删除</el-button>
-            <el-dialog
-              :center="true"
-              :title="scope.row.blogTitle"
-              :visible.sync="dialogVisible"
-              width="30%"
-            >
-              <ChildComment v-bind:comment="scope.row"></ChildComment>
-              <span v-if="answerWho !== ''" slot="footer" class="dialog-footer">
-                <el-input
-                  style="width:90%;"
-                  type="textarea"
-                  :rows="2"
-                  placeholder="回复"
-                  v-model="answerContent"
-                ></el-input>
-                <el-button size="small" type="success" icon="el-icon-check" circle></el-button>
-              </span>
-            </el-dialog>
+            <el-button size="mini" type="danger" @click="handleDelete(scope.$index)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
+      <el-dialog
+        :center="true"
+        :title="dialogData.title"
+        :visible.sync="dialogVisible"
+        :before-close="closeDialogView"
+        width="30%"
+      >
+        <ChildComment v-bind:comment="dialogData"></ChildComment>
+        <span v-if="answerWho !== ''" slot="footer" class="dialog-footer">
+          <el-input
+            style="width:90%;"
+            type="textarea"
+            :rows="2"
+            placeholder="回复"
+            v-model="answerContent"
+          ></el-input>
+          <el-button size="small" type="success" icon="el-icon-check" circle></el-button>
+        </span>
+      </el-dialog>
     </el-main>
   </div>
 </template>
 <script>
 import ChildComment from "../components/ChildComment";
 import bus from "../utils/bus";
+import { getCommentList } from "../api/api.js";
 export default {
   data() {
     return {
-      dialogVisible: false,
       pageInfo: {
         name: "评论列表",
         parent: "/home"
       },
+      dialogVisible: false,
+      dialogData: {},
       answerContent: "",
       answerWho: "",
-      tableData: [
-        {
-          cid: 1,
-          nickname: "nichname",
-          email: "1110@qq.com",
-          createDate: "2016-05-02",
-          blogTitle: "blog title example1",
-          content: "评论",
-          children: [
-            {
-              cid: 1,
-              nickname: "nichname",
-              email: "1110@qq.com",
-              createDate: "2016-05-02",
-              blogTitle: "blog title example1",
-              content: "评论的评论",
-              children: [
-                {
-                  cid: 1,
-                  nickname: "nichname",
-                  email: "1110@qq.com",
-                  createDate: "2016-05-02",
-                  blogTitle: "blog title example1",
-                  content: "评论的评论的评论",
-                  children: []
-                }
-              ]
-            },
-            {
-              cid: 1,
-              nickname: "nichname",
-              email: "1110@qq.com",
-              createDate: "2016-05-02",
-              blogTitle: "blog title example1",
-              content: "评论的评论",
-              children: [
-                {
-                  cid: 1,
-                  nickname: "nichname",
-                  email: "1110@qq.com",
-                  createDate: "2016-05-02",
-                  blogTitle: "blog title example1",
-                  content: "评论的评论的评论",
-                  children: []
-                }
-              ]
-            }
-          ]
-        }
-      ]
+      commentList: []
     };
   },
   watch: {
@@ -124,10 +78,43 @@ export default {
     bus.$on("sendParent", comment => {
       this.answerWho = comment.nickname;
       this.answerContent = "@" + this.answerWho + ":";
-      console.log(comment);
+    });
+    getCommentList().then(resp => {
+      if (resp.data.success) {
+        this.commentList = resp.data.data;
+      }
     });
   },
-  methods: {},
+  methods: {
+    showDialogView(row) {
+      this.dialogData = row;
+      this.dialogVisible = true;
+    },
+    closeDialogView() {
+      this.answerWho = "";
+      this.dialogVisible = false;
+    },
+    handleDelete(index) {
+      this.$confirm("此操作将会删除该评论, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.commentList.splice(index, 1);
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    }
+  },
   components: {
     ChildComment
   }
